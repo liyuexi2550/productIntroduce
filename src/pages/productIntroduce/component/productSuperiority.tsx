9ffe14b11e99d8ee500f255fea677f17e36dc4bb
liyuexi2550 <709284232@qq.com>
2021-07-09 12:32:40 +0800

    second commit

--- src/pages/productIntroduce/component/productSuperiority.tsx
+++ src/pages/productIntroduce/component/productSuperiority.tsx
@@ -1,16 +1,38 @@
 import styles from './videoIntroduction.less';
 import Mytitle from './title'
 
+let url = [
+    {
+        clsName:"{styles.gridItem1}",
+        src:"https://www.xiaomingtea.com/template/default/index/images/grid1.png"
+    },
+    {
+        clsName:"{styles.gridItem2}",
+        src:"https://www.xiaomingtea.com/template/default/index/images/grid2.png"
+    },
+    {
+        clsName:"{styles.gridItem3}",
+        src:"https://www.xiaomingtea.com/template/default/index/images/grid3.png"
+    },
+    {
+        clsName:"{styles.gridItem4}",
+        src:"https://www.xiaomingtea.com/template/default/index/images/grid4.png"
+    }
+]
 export default function ProductSuperiority() {
     return(
          <div className={styles.productSuperiority}>
              <div className={styles.productAndDescription}>
                  <Mytitle Name="产品优势" EnglishName="Product Superiority"></Mytitle>
                  <div className={styles.grid}>
-                     <img className={styles.gridItem1} src="https://www.xiaomingtea.com/template/default/index/images/grid1.png"/>
-                     <img className={styles.gridItem2} src="https://www.xiaomingtea.com/template/default/index/images/grid2.png"/>
-                     <img className={styles.gridItem3} src="https://www.xiaomingtea.com/template/default/index/images/grid3.png"/>
-                     <img className={styles.gridItem4} src="https://www.xiaomingtea.com/template/default/index/images/grid4.png"/>
+                     {
+                            url.map((item)=>{
+                                return(
+                                    <img className={item.clsName} src={item.src}   /> 
+                                )
+                            })
+                     }
+                
                  </div>
              </div>
      </div>
